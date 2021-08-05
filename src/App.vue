c58a7c5c738b5e1452bc1bdc2addaefac657771c
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2021-08-05 06:11:56 +0200

    add tiptap

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import VueComponent from './Extension.js'
+
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        Document,
+        Paragraph,
+        Text,
+        VueComponent,
+      ],
+      content: `
+        <p>
+          This is a radically reduced version of tiptap. It has support for a document, with paragraphs and text. That’s it. It’s probably too much for real minimalists though.
+        </p>
+        <p>        <vue-component count="0"></vue-component></p>
+                <vue-component count="0"></vue-component>
+        <p>
+          The paragraph extension is not really required, but you need at least one node. Sure, that node can be something different.
+        </p>
+      `,
+    })
+  },
+
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
+
+<style lang="scss">
+/* Basic editor styles */
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+  }
+}
+</style>
---
+++ src/Component.vue
@@ -0,0 +1,28 @@
+<template>
+  <node-view-wrapper as="span" class="vue-component">
+    <span class="label">Vue Component</span>
+  </node-view-wrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-2'
+
+export default {
+  components: {
+    NodeViewWrapper,
+  },
+
+  props: nodeViewProps,
+
+  methods: {
+    increase() {
+      this.updateAttributes({
+        count: this.node.attrs.count + 1,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
